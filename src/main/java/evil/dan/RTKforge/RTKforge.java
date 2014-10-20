1c0ed8d726418410599ea997f916db9a39a54a1b
evil-dan2wik <dan2wik@gmail.com>
2014-10-20 21:54:27 +1000

    Basic heart beat
    Possibly working.

--- src/main/java/evil/dan/RTKforge/RTKforge.java
+++ src/main/java/evil/dan/RTKforge/RTKforge.java
@@ -1,9 +1,14 @@
 package evil.dan.RTKforge;
 
+import net.minecraft.command.ICommandManager;
+import net.minecraft.command.ServerCommandManager;
 import net.minecraft.init.Blocks;
+import net.minecraft.server.MinecraftServer;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import evil.dan.RTKforge.server.CommandRTPing;
 
 @Mod(modid = RTKforge.MODID, version = RTKforge.VERSION)
 public class RTKforge
@@ -16,5 +21,15 @@ public class RTKforge
     {
 		// some example code
         System.out.println("Hello, I am looking for a young man known as Remote Toolkit, have you seen him?");
+    }
+    
+    @EventHandler
+    public void serverStart(FMLServerStartingEvent event)
+    {
+    	System.out.println("Hooking RTK commands");
+         MinecraftServer server = MinecraftServer.getServer();
+         ICommandManager command = server.getCommandManager();
+         ServerCommandManager manager = (ServerCommandManager) command;
+         manager.registerCommand(new CommandRTPing());
     }
 }
--- src/main/java/evil/dan/RTKforge/server/CommandRTPing.java
+++ src/main/java/evil/dan/RTKforge/server/CommandRTPing.java
@@ -1,5 +1,45 @@
 package evil.dan.RTKforge.server;
 
-public class CommandRTPing {
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandServerKick;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
 
+public class CommandRTPing extends CommandBase
+{
+	@Override
+	public String getCommandName()
+	{
+		return "RTPING";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender icommandsender)
+	{
+		return null;
+	}
+
+	@Override
+	public void processCommand(ICommandSender icommandsender, String[] astring)
+	{
+		if (icommandsender.getCommandSenderName() == "Server") {
+			System.out.println("RTPONG++");
+		} else {
+			CommandBase kick = new CommandServerKick();
+			kick.processCommand(icommandsender, new String[] { icommandsender.getCommandSenderName() });
+		}
+	}
+
+	@Override
+	public int compareTo(Object o)
+	{
+		if (o instanceof ICommand)
+		{
+			return this.compareTo((ICommand) o);
+		}
+		else
+		{
+			return 0;
+		}
+	}
 }
